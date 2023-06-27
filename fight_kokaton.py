f8ff0eaf751d70b4446302ac85db5caa27f86837
kawanishi <c0b2204663@edu.teu.ac.jp>
2023-06-27 16:34:16 +0900

    練習問題3

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -163,6 +163,8 @@ def main():
             if bomb.rct.colliderect(beam.rct):
                 bomb = None
                 beam = None
+                bird.change_img(6, screen)
+                pg.display.update()              
 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
